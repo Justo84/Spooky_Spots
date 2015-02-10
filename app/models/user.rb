d1058cc8ff2f893a37c6d51f8e16b64cff1b5e36
Justo84 <justin.d.egan@gmail.com>
2015-02-10 10:16:35 -0500

    Basic functionality of forum opperating

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
   def is_admin?
     role == 'admin'
   end
+
+  def forem_name
+    name
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
